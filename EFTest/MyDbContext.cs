72c2ae4919890792b4eb114f40a216f0b67e2647
Geoff Battye <geoff.battye@octopus.com>
2024-05-20 22:13:44 +1000

    Initial commit.

---
+++ EFTest/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<MyEntity> Entities { get; set; }
+    }
+}
